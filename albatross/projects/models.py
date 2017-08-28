45511877ed32af58018dbdf3f182055e3ec671b0
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-28 17:35:27 -0400

    Add created at and updated at fields to projects models.

--- albatross/projects/models.py
+++ albatross/projects/models.py
@@ -3,7 +3,15 @@ from django.db.models import Sum
 from django.db.models.functions import Coalesce
 
 
-class Project(models.Model):
+class CommonInfo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Project(CommonInfo):
     name = models.CharField(max_length=200)
 
     @property
@@ -17,7 +25,7 @@ class Project(models.Model):
         return aggregate_results['sum']
 
 
-class Category(models.Model):
+class Category(CommonInfo):
     name = models.CharField(max_length=200)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='categories')
 
@@ -32,7 +40,7 @@ class Category(models.Model):
         return aggregate_results['sum']
 
 
-class Item(models.Model):
+class Item(CommonInfo):
     actual = models.DecimalField(max_digits=7, decimal_places=1)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     description = models.CharField(max_length=200)
