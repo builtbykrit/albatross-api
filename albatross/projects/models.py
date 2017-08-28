ce6cb0e4c3494a3566e6cbae5a6b6ece7831623e
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-28 15:31:45 -0400

    Add tests for empty category

--- albatross/projects/models.py
+++ albatross/projects/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
 
 
 class Project(models.Model):
@@ -7,11 +9,21 @@ class Project(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='categories')
+
+    @property
+    def actual(self):
+        aggregate_results = self.items.aggregate(sum=Coalesce(Sum('actual'), 0))
+        return aggregate_results['sum']
+
+    @property
+    def estimated(self):
+        aggregate_results = self.items.aggregate(sum=Coalesce(Sum('estimated'), 0))
+        return aggregate_results['sum']
 
 
 class Item(models.Model):
     actual = models.DecimalField(max_digits=7, decimal_places=1)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     description = models.CharField(max_length=200)
     estimated = models.DecimalField(max_digits=7, decimal_places=1)
--- albatross/projects/tests.py
+++ albatross/projects/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Project, Category
+
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        project = Project.objects.create(name='My Project')
+        Category.objects.create(name='Design', project=project)
+
+    def test_no_items_in_category(self):
+        category = Category.objects.get(name='Design')
+        self.assertEqual(category.items.all().count(), 0)
+        self.assertEqual(category.actual, 0)
+        self.assertEqual(category.estimated, 0)
