fa23231a1e07bc7e1c5b12a537a5fc3c15348222
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-29 12:18:41 -0400

    Include categories in projects response.

--- albatross/projects/serializers.py
+++ albatross/projects/serializers.py
@@ -2,13 +2,20 @@ from rest_framework import serializers
 from .models import Category, Project
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ('id', 'name', 'categories', 'estimated', 'actual', 'created_at', 'updated_at')
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'items', 'estimated', 'actual', 'created_at', 'updated_at')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    included_serializers = {
+        'categories': CategorySerializer
+    }
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'categories', 'estimated', 'actual', 'created_at', 'updated_at')
+
+    class JSONAPIMeta:
+        included_resources = ['categories']
