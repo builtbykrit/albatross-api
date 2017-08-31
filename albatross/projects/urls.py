d3a674f5caac6572547570b7dad71d3976c7d801
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-31 11:40:26 -0400

    Revert "Fix projects app urls."
    This reverts commit 5314b5bae6bb88bd35986aea4302dc34d30558b4.

--- albatross/projects/urls.py
+++ albatross/projects/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import include, url
 from .views import CategoryViewSet, ItemViewSet, ProjectViewSet
 
 router = SimpleRouter()
-router.register(r'^projects/$', ProjectViewSet, 'project')
-router.register(r'^categories/$', CategoryViewSet)
-router.register(r'^items/$', ItemViewSet)
+router.register(r'projects', ProjectViewSet, 'project')
+router.register(r'categories', CategoryViewSet)
+router.register(r'items', ItemViewSet)
 
 
 urlpatterns = router.urls
