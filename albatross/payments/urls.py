77aec92efd7445a25e4c7b86daf812a999998468
Bill Brower <browerw@email.sc.edu>
2017-10-09 18:29:42 -0400

    Added a slash to the end of the GET /payments/details endpoint (so it's GET /payments/details/) because all of the other payments endpoints ended with slashes.

--- albatross/payments/urls.py
+++ albatross/payments/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^change-card-token/$', views.ChangeCardTokenView.as_view(), name='stripe-change-card-token'),
-    url(r'^details$', views.CurrentCustomerDetailView.as_view(), name='payments-details'),
+    url(r'^details/$', views.CurrentCustomerDetailView.as_view(), name='payments-details'),
     url(r'^subscription/$', views.SubscriptionView.as_view(), name='payments-subscription'),
     url(r'^subscription/cancel/$', views.CancelView.as_view(), name='payments-subscription-cancel'),
     url(r'^webhook/$', views.WebhookView.as_view(), name='stripe-webhook'),
