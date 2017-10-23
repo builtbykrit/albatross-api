96a3d2320851da12b8dce0de4e91d38e23606cdd
Kevin Hoffman <kehoffman3@gmail.com>
2017-10-23 14:23:05 -0400

    Signup user to albatross newsletter if requested.

--- albatross/registration/views.py
+++ albatross/registration/views.py
@@ -1,5 +1,10 @@
+import hashlib
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
+from mailchimp3 import MailChimp
 from rest_framework import status
 from rest_framework.exceptions import APIException, ValidationError
 from rest_framework.generics import CreateAPIView
@@ -9,7 +14,6 @@ from rest_framework_json_api import renderers
 from .hooks import hookset
 from .models import SignupCode
 from .serializers import UserRegistrationSerializer
-
 
 MESSAGE_STRINGS = hookset.get_message_strings()
 
@@ -31,6 +35,25 @@ class UserRegistrationView(CreateAPIView):
             self.perform_create(serializer)
 
             user = serializer.instance
+
+            subscribe_to_newsletter = request.data.get('subscribe_to_newsletter', False)
+            if subscribe_to_newsletter and not settings.DEBUG:
+                email_address = user.email_address
+                mailchimp_client = MailChimp(
+                    mc_user=os.environ.get('MAILCHIMP_USER_NAME'),
+                    mc_secret=os.environ.get('MAILCHIMP_API_KEY')
+                )
+                mailchimp_client.lists.members.create_or_update(
+                    list_id=os.environ.get('MAILCHIMP_LIST_ID'),
+                    subscriber_hash=hashlib.md5(
+                        email_address.encode()
+                    ).hexdigest(),
+                    data={
+                        'email_address': email_address,
+                        'status_if_new': 'subscribed',
+                    }
+                )
+
             if signup_code:
                 signup_code.use(user)
 
